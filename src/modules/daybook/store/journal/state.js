cf9236aac34808ad9a16a52a2c0826f0a8b0fcd8
daniguillermo <dguillermo.supervisa@gmail.com>
2023-09-01 08:57:53 -0500

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), // 1232138764
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Do eu amet nulla velit magna labore tempor ad. Incididunt aliqua duis minim sit nisi esse qui nulla. Sit dolore sit duis deserunt minim do duis ex Lorem deserunt sint ipsum.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 1000, // 1232138764
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Veniam consequat sint et irure voluptate aute. Aliquip pariatur qui proident nisi eu dolore excepteur magna Lorem laborum nulla dolor ipsum. Nulla dolor sunt eiusmod excepteur est sit. Eiusmod aute laboris velit duis officia non mollit mollit labore sunt cupidatat magna do. Ex elit sint eiusmod eiusmod pariatur irure enim velit laborum do sint laboris cupidatat.',
+            picture: null, // https://
+        },
+        {
+            id: new Date().getTime() + 2000, // 1232138764
+            date: new Date().toDateString(), // sat 23, julio
+            text: 'Veniam ullamco enim proident qui ipsum officia duis officia anim incididunt laboris est. Aute esse sunt laboris nulla tempor enim proident. Enim incididunt in duis minim id duis proident aliqua tempor pariatur. Ex irure in reprehenderit nostrud commodo nulla Lorem dolore sunt eiusmod in elit consectetur cillum. Anim nulla sunt consectetur aliqua laboris mollit id et culpa mollit. Quis consequat excepteur cupidatat voluptate eu Lorem sit.',
+            picture: null, // https://
+        },
+    ]
+})
